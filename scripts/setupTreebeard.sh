07757da862916af271b189a06cc0b5bcf79e7af8
Ashwin Prasad <ashwin.prasad@gmail.com>
2022-07-27 20:40:17 +0530

    Fix setup script

--- scripts/setupTreebeard.sh
+++ scripts/setupTreebeard.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "Cloning Treebeard git repo ..."
 cd llvm-project/mlir/examples/
-git clone https://github.com/asprasad/tree-heavy.git
-mkdir tree-heavy/build
-cd tree-heavy/build
-git checkout micro-artifact
+git clone https://github.com/asprasad/treebeard.git
+mkdir treebeard/build
+cd treebeard/build
 bash ../scripts/gen.sh
 cmake --build .
