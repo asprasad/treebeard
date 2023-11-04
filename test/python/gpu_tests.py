f1f41b9f2e6a388f0d0ebf9d86606a9039ebce5b
Ashwin Prasad <ashwin.prasad@gmail.com>
2023-11-04 13:04:24 +0530

    Add one row at a time GPU python tests

--- test/python/gpu_tests.py
+++ test/python/gpu_tests.py
@@ -12,9 +12,21 @@ def SimpleGPUSchedule(schedule: treebeard.Schedule, rows_per_threadblock: int):
   threadBlockIndex.set_gpu_dimension(treebeard.GPUConstruct.Grid, treebeard.Dimension.X)
   threadIndex.set_gpu_dimension(treebeard.GPUConstruct.ThreadBlock, treebeard.Dimension.X)
 
-def run_custom_schedule(test_name, rep, schedule_manipulator, tile_size=1):
-  defaultTileSize8Options = treebeard.CompilerOptions(200, tile_size)
-  defaultTileSize8MulticlassOptions = treebeard.CompilerOptions(200, tile_size)
+def OneRowAtATimeGPUSchedule(schedule: treebeard.Schedule, rows_per_threadblock: int, rows_per_thread: int):
+  batchIndex = schedule.GetBatchIndex()
+  threadBlockIndex = schedule.NewIndexVariable("b0")
+  threadIndexTemp = schedule.NewIndexVariable("b1_temp")
+  threadIndex = schedule.NewIndexVariable("b1_outer")
+  perThreadIndex = schedule.NewIndexVariable("b1_inner")
+  schedule.Tile(batchIndex, threadBlockIndex, threadIndexTemp, tileSize=rows_per_threadblock)
+  schedule.Tile(threadIndexTemp, threadIndex, perThreadIndex, tileSize=rows_per_thread)
+  threadBlockIndex.set_gpu_dimension(treebeard.GPUConstruct.Grid, treebeard.Dimension.X)
+  threadIndex.set_gpu_dimension(treebeard.GPUConstruct.ThreadBlock, treebeard.Dimension.X)
+
+
+def run_custom_schedule(test_name, rep, schedule_manipulator, tile_size=1, batch_size=200):
+  defaultTileSize8Options = treebeard.CompilerOptions(batch_size, tile_size)
+  defaultTileSize8MulticlassOptions = treebeard.CompilerOptions(batch_size, tile_size)
   defaultTileSize8MulticlassOptions.SetReturnTypeWidth(8)
   defaultTileSize8MulticlassOptions.SetReturnTypeIsFloatType(False)
 
@@ -31,8 +43,15 @@ def RunSimpleGPUScheduleTests():
   run_custom_schedule("simple_gpu_schedule-sparse", "gpu_sparse", gpu_schedule_20rows)
   run_custom_schedule("simple_gpu_schedule-reorg", "gpu_reorg", gpu_schedule_20rows)  
 
+def RunOneRowAtATimeGPUScheduleTests():
+  gpu_schedule = partial(OneRowAtATimeGPUSchedule, rows_per_threadblock=40, rows_per_thread=2)
+  run_custom_schedule("row-at-a-time_gpu_schedule-array", "gpu_array", gpu_schedule)
+  run_custom_schedule("row-at-a-time_schedule-sparse", "gpu_sparse", gpu_schedule)
+  run_custom_schedule("row-at-a-time_schedule-reorg", "gpu_reorg", gpu_schedule)  
+
 def run_all_tests():
   RunSimpleGPUScheduleTests()
+  RunOneRowAtATimeGPUScheduleTests()
 
 if __name__ == "__main__":
   run_all_tests()
