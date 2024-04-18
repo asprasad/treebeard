dce7ff33f9df0040927373501f614f8abe659c89
Ashwin Prasad <ashwin.prasad@gmail.com>
2024-04-18 20:54:21 +0530

    Add cummulative freq plots for random model speedups

--- results/python/analyze_random_model_benchmark_results.py
+++ results/python/analyze_random_model_benchmark_results.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib.pyplot as plt
 
 batch_size = 512
 # file_path = "/home/ashwin/mlir-build/llvm-project/mlir/examples/treebeard/results/thedeep/gpu/random_models/20240320/random_cmp_output_mp.txt"
@@ -52,30 +53,47 @@ import scipy.stats as stats
 # print(stats.gmean(df["tb_kernel_speedup"]))
 #print(df["tb_kernel_speedup"].prod() ** (1.0 / df.shape[0]))
 
-for depth in range(6, 9):
-    # get entries with tree_depth == depth
-    df_depth = df[df["tree_depth"] == depth]
-    # print(df_depth.shape)
+# for depth in range(6, 9):
+#     # get entries with tree_depth == depth
+#     df_depth = df[df["tree_depth"] == depth]
+#     # print(df_depth.shape)
 
-    # plot a 3-d graph of num_trees, num_features and tb_kernel_speedup
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
+#     # plot a 3-d graph of num_trees, num_features and tb_kernel_speedup
+#     import matplotlib.pyplot as plt
+#     from mpl_toolkits.mplot3d import Axes3D
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
 
-    # 3D scatter plot
-    # ax.scatter3D(df_depth["num_trees"], df_depth["num_features"], df_depth["tb_kernel_speedup"], c='red')
-    surf = ax.plot_trisurf(df_depth["num_trees"], df_depth["num_features"], df_depth["tb_kernel_speedup"], cmap='viridis')
+#     # 3D scatter plot
+#     # ax.scatter3D(df_depth["num_trees"], df_depth["num_features"], df_depth["tb_kernel_speedup"], c='red')
+#     surf = ax.plot_trisurf(df_depth["num_trees"], df_depth["num_features"], df_depth["tb_kernel_speedup"], cmap='viridis')
 
-    ax.set_xlabel('Number of Trees')
-    ax.set_ylabel('Number of Features')
-    ax.set_zlabel('Speedup')
-    min_speedup = df_depth["tb_kernel_speedup"].min()
-    max_speedup = df_depth["tb_kernel_speedup"].max()
-    geomean_speedup = stats.gmean(df_depth["tb_kernel_speedup"])
-    # limit the speedup values to 1 decimal digit
-    plt.title(f"Min:{min_speedup:.2f}x    Max:{max_speedup:.2f}x    Geomean:{geomean_speedup:.2f}x")
-    plt.colorbar(surf, label='Speedup')
-    plt.savefig(f'kernel_speedup_b{batch_size}_depth{depth}.png')
-    # plt.show()
+#     ax.set_xlabel('Number of Trees')
+#     ax.set_ylabel('Number of Features')
+#     ax.set_zlabel('Speedup')
+#     min_speedup = df_depth["tb_kernel_speedup"].min()
+#     max_speedup = df_depth["tb_kernel_speedup"].max()
+#     geomean_speedup = stats.gmean(df_depth["tb_kernel_speedup"])
+#     # limit the speedup values to 1 decimal digit
+#     plt.title(f"Min:{min_speedup:.2f}x    Max:{max_speedup:.2f}x    Geomean:{geomean_speedup:.2f}x")
+#     plt.colorbar(surf, label='Speedup')
+#     plt.savefig(f'kernel_speedup_b{batch_size}_depth{depth}.png')
+#     # plt.show()
+
+# Sort data in ascending order
+sorted_values = df["tb_kernel_speedup"].sort_values(ascending=True)
+
+# Calculate relative cumulative frequencies
+percentages = sorted_values.rank(pct=True)
+plt.plot(sorted_values, percentages) #, marker='o', linestyle='-')
+plt.xlabel('SilvanForge Speedup vs RAPIDS', fontsize=14)
+plt.ylabel('Cumulative Frequency', fontsize=14)
+plt.tick_params(labelsize=14)
+if batch_size == 4096:
+  plt.axis([0, 7, -0.05, 1.05])
+# else:
+#   plt.axis([0, 9, -0.05, 1.05])
+# plt.title('Ogive (CDF) of Values')
+plt.grid(True)
+plt.savefig(f'ogive_b{batch_size}.png')
