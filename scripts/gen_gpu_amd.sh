dd7029cf1192242bee365ea3b6aea30686b43b6d
asprasad <ashwin.prasad@gmail.com>
2024-08-21 10:32:56 +0530

    Add case for Ninja not being installed

--- scripts/gen_gpu_amd.sh
+++ scripts/gen_gpu_amd.sh
@@ -24,12 +24,24 @@ echo "Using cmake command : $CMAKE"
 echo "Using MLIR_BUILD : $MLIR_BUILD"
 echo "Using configuration : $CONFIG"
 
-# run this from the build directory
-$CMAKE  .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
-       -DMLIR_DIR=$LLVM_DIR/$MLIR_BUILD/lib/cmake/mlir \
-       -DLLVM_BUILD_DIRECTORY=$LLVM_DIR/$MLIR_BUILD/ \
-       -DCMAKE_BUILD_TYPE=$CONFIG \
-       -DAMD_GPU_SUPPORT=ON
-#      -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
-#      -DLLVM_ENABLE_LLD=ON
-#      -DCMAKE_CXX_FLAGS="-std=c++17"
+if command -v ninja > /dev/null 2>&1; then
+    # run this from the build directory
+    $CMAKE  -G Ninja .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+        -DMLIR_DIR=$LLVM_DIR/$MLIR_BUILD/lib/cmake/mlir \
+        -DLLVM_BUILD_DIRECTORY=$LLVM_DIR/$MLIR_BUILD/ \
+        -DCMAKE_BUILD_TYPE=$CONFIG \
+        -DAMD_GPU_SUPPORT=ON
+    #      -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+    #      -DLLVM_ENABLE_LLD=ON
+    #      -DCMAKE_CXX_FLAGS="-std=c++17"
+else
+    # run this from the build directory
+    $CMAKE  .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+        -DMLIR_DIR=$LLVM_DIR/$MLIR_BUILD/lib/cmake/mlir \
+        -DLLVM_BUILD_DIRECTORY=$LLVM_DIR/$MLIR_BUILD/ \
+        -DCMAKE_BUILD_TYPE=$CONFIG \
+        -DAMD_GPU_SUPPORT=ON
+    #      -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+    #      -DLLVM_ENABLE_LLD=ON
+    #      -DCMAKE_CXX_FLAGS="-std=c++17"
+fi
--- scripts/setupSilvanForgeAMD.sh
+++ scripts/setupSilvanForgeAMD.sh
@@ -4,9 +4,13 @@ cd llvm-project/
 git checkout release/16.x
 mkdir build
 cd build
-cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS="llvm;clang;lld;mlir;openmp" -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON -DMLIR_ENABLE_ROCM_RUNNER=ON -DMLIR_INCLUDE_INTEGRATION_TESTS=ON
-cmake --build .
-
+if command -v ninja > /dev/null 2>&1; then
+    cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS="llvm;clang;lld;mlir;openmp" -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON -DMLIR_ENABLE_ROCM_RUNNER=ON -DMLIR_INCLUDE_INTEGRATION_TESTS=ON
+    cmake --build .
+else
+    cmake ../llvm -DLLVM_ENABLE_PROJECTS="llvm;clang;lld;mlir;openmp" -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON -DMLIR_ENABLE_ROCM_RUNNER=ON -DMLIR_INCLUDE_INTEGRATION_TESTS=ON
+    cmake --build . -j16
+fi
 # Clone and build Treebeard
 cd ../mlir/examples
 git clone https://github.com/asprasad/treebeard.git
@@ -19,6 +23,10 @@ git checkout move-pvt-to-os
 mkdir build
 cd build
 bash ../scripts/gen_gpu_amd.sh
-cmake --build .
+if command -v ninja > /dev/null 2>&1; then
+    cmake --build .
+else
+    cmake --build . -j16
+fi
 
 
